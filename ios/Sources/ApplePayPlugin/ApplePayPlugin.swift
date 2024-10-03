b6713ee2a5ba8c2049af648ac9211fedf05bebb4
Paul Mojica <paulmojicatech@gmail.com>
2024-10-03 12:26:57 -0400

    add recurring payments add definitions

--- ios/Sources/ApplePayPlugin/ApplePayPlugin.swift
+++ ios/Sources/ApplePayPlugin/ApplePayPlugin.swift
@@ -31,60 +31,97 @@ public class ApplePayPlugin: CAPPlugin, CAPBridgedPlugin, PKPaymentAuthorization
         ])
     }
 
-    @objc func showApplePaySheet(_ call: CAPPluginCall) {
-        guard PKPaymentAuthorizationViewController.canMakePayments() else {
-            call.reject("Apple Pay is not available on this device")
+  @objc func showApplePaySheet(_ call: CAPPluginCall) {
+    guard PKPaymentAuthorizationViewController.canMakePayments() else {
+      call.reject("Apple Pay is not available on this device")
+      return
+    }
+    
+    guard let merchantIdentifier = call.getString("merchantId"),
+          let countryCode = call.getString("countryCode"),
+          let currencyCode = call.getString("currencyCode"),
+          let supportedNetworks = call.getArray("supportedNetworks", String.self),
+          let paymentSummaryItems = call.getArray("paymentSummaryItems", [String: Any].self) else {
+      call.reject("Missing required parameters")
+      return
+    }
+    let paymentRequest = PKPaymentRequest()
+    paymentRequest.merchantIdentifier = merchantIdentifier
+    paymentRequest.supportedNetworks = supportedNetworks.map { PKPaymentNetwork(rawValue: $0) }
+    paymentRequest.merchantCapabilities = .capability3DS
+    paymentRequest.countryCode = countryCode
+    paymentRequest.currencyCode = currencyCode
+    
+    // Add payment summary items
+    if let singlePayments = call.getArray("paymentSummaryItems", [String: Any].self) {
+      paymentRequest.paymentSummaryItems = singlePayments.compactMap{ item in
+        guard let label = item["label"] as? String,
+              let amountString = item["amount"] as? String,
+              let amount = NSDecimalNumber(string: amountString) as? NSDecimalNumber else {
+          return nil
+        }
+        return PKPaymentSummaryItem(label: label, amount: amount)
+      }
+    }
+    // Add recurring summary items if provided
+    else if let recurringSummaryItems = call.getArray("recurringSummaryItems", [String: Any].self) {
+      if #available(iOS 16.0, *) {
+        var recurringPaymentsToAdd: [PKRecurringPaymentSummaryItem] = []
+        recurringSummaryItems.forEach{ item in
+          guard let label = item["label"] as? String,
+                let amountString = item["amount"] as? String,
+                let amount = NSDecimalNumber(string: amountString) as? NSDecimalNumber,
+                let startDateString = item["startDate"] as? String,
+                let startDate = ISO8601DateFormatter().date(from: startDateString),
+                let intervalUnit = item["intervalUnit"] as? NSCalendar.Unit,
+                let intervalCount = item["intervalCount"] as? Int else {
+            call.reject("Invalid recurring payment item")
             return
+          }
+          
+          let recurringItem = PKRecurringPaymentSummaryItem(label: label, amount: amount)
+          recurringItem.startDate = startDate
+          recurringItem.intervalUnit = intervalUnit
+          recurringItem.intervalCount = intervalCount
+          guard let url = item["managementURL"] as? URL else {
+            call.reject("Invalid recurring payment item")
+            return;
+          }
+          let recurringPaymentRequest = PKRecurringPaymentRequest(paymentDescription:label, regularBilling:recurringItem, managementURL: url)
+          paymentRequest.recurringPaymentRequest = recurringPaymentRequest
         }
-
-        guard let merchantIdentifier = call.getString("merchantId"),
-              let countryCode = call.getString("countryCode"),
-              let currencyCode = call.getString("currencyCode"),
-              let supportedNetworks = call.getArray("supportedNetworks", String.self),
-              let paymentSummaryItems = call.getArray("paymentSummaryItems", [String: Any].self) else {
-                call.reject("Missing required parameters")
-                return
-              }
-        let paymentRequest = PKPaymentRequest()
-        paymentRequest.merchantIdentifier = merchantIdentifier
-        paymentRequest.supportedNetworks = supportedNetworks.map { PKPaymentNetwork(rawValue: $0) }
-        paymentRequest.merchantCapabilities = .capability3DS
-        paymentRequest.countryCode = countryCode
-        paymentRequest.currencyCode = currencyCode
-        paymentRequest.paymentSummaryItems = paymentSummaryItems.compactMap { item in
-            guard let label = item["label"] as? String,
-                  let amountString = item["amount"] as? String,
-                  let amount = NSDecimalNumber(string: amountString) as? NSDecimalNumber else {
-                    return nil
-                }
-            return PKPaymentSummaryItem(label: label, amount: amount)
-        }
-
-        guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) else {
-            call.reject("Failed to create payment authorization view controller")
-            return
-        }
-        paymentVC.delegate = self
-        DispatchQueue.main.async {
-            self.bridge?.viewController?.present(paymentVC, animated: true, completion: nil)
-        }
-        call.resolve([
-            "success": true
-        ])
+        
+      } else {
+        call.reject("Recurring payments are not supported on version of iOS")
+      }
+        
     }
-
-    public func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
-        // Handle the authorized payment here
-        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
-
-        // Notify the bridge that the payment was successful
-        self.notifyListeners("paymentSuccess", data: [
-            "status": "success",
-            "paymentData": payment.token.paymentData.base64EncodedString()
-        ])
+      
+    guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) else {
+      call.reject("Failed to create payment authorization view controller")
+      return
     }
-
-    public func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
-        controller.dismiss(animated: true, completion: nil)
+    paymentVC.delegate = self
+    DispatchQueue.main.async {
+      self.bridge?.viewController?.present(paymentVC, animated: true, completion: nil)
     }
+    call.resolve([
+      "success": true
+    ])
+  }
+
+  public func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+      // Handle the authorized payment here
+      completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+
+      // Notify the bridge that the payment was successful
+      self.notifyListeners("paymentSuccess", data: [
+          "status": "success",
+          "paymentData": payment.token.paymentData.base64EncodedString()
+      ])
+  }
+
+  public func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+      controller.dismiss(animated: true, completion: nil)
+  }
 }
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,15 @@
+import type { ListenerCallback, PluginListenerHandle } from '@capacitor/core';
+
 export interface PaymentSummaryItem {
   label: string;
   amount: string;
+}
+
+export interface RecurrentPaymentSummaryItem extends PaymentSummaryItem {
+  startDate: string;
+  intervalUnit: string;
+  managementUrl: string;
+  intervalCount?: number;
 }
 export interface ApplePayRequestOptions {
   merchantId: string;
@@ -8,12 +17,13 @@ export interface ApplePayRequestOptions {
   currencyCode: string;
   supportedNetworks: string[];
   merchantCapabilities: string[];
-  paymentSummaryItems: PaymentSummaryItem[];
+  paymentSummaryItems?: PaymentSummaryItem[];  
+  recurringSummaryItems?: RecurrentPaymentSummaryItem[];
 }
 
 export interface ApplePayPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   canMakePayments(): Promise<{ canMakePayments: boolean }>;
   showApplePaySheet(options: ApplePayRequestOptions): Promise<{ success: boolean }>;
-
+  addListener(eventName: string, listenerFunc: ListenerCallback): Promise<PluginListenerHandle>;
 }
